6217f74c7cc7ce8b0db764243b62d0c205dafbe7
My10xDev <johan@openwebset.com>
2024-12-09 19:07:56 +0000

    dynamic array

--- main.c
+++ main.c
@@ -3,6 +3,34 @@
 #include <math.h>
 #include <time.h>
 
+typedef struct {
+  int *array;
+  size_t used;
+  size_t size;
+} Array;
+
+void initArray(Array *a, size_t initialSize) {
+  a->array = malloc(initialSize * sizeof(int));
+  a->used = 0;
+  a->size = initialSize;
+}
+
+void insertArray(Array *a, int element) {
+  // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
+  // Therefore a->used can go up to a->size 
+  if (a->used == a->size) {
+    a->size *= 2;
+    a->array = realloc(a->array, a->size * sizeof(int));
+  }
+  a->array[a->used++] = element;
+}
+
+void freeArray(Array *a) {
+  free(a->array);
+  a->array = NULL;
+  a->used = a->size = 0;
+}
+
 // Function to generate a random float between -1 and 1
 float random_uniform() {
     return ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
@@ -46,6 +74,17 @@ int main(){
     float x = 1.352F;
 
     printf("SIGMOID -> %0.15f\n", atanhf(x));
+    printf("HELOOOOOOOOOOOOOOOOOOOOOO\n\n\n");
+
+    Array a;
+int i;
+
+initArray(&a, 5);  // initially 5 elements
+for (i = 0; i < 100; i++)
+  insertArray(&a, i);  // automatically resizes as necessary
+printf("%d\n", a.array[9]);  // print 10th element
+printf("%ld\n", a.used);  // print number of elements
+freeArray(&a);
 
     return 0;
 }
